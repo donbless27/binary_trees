34d53749f0a9c007a2314750ab6d8e02b0181595
donbless27 <donbless0527@gmail.com>
2023-11-01 05:44:18 -0700

    Binary Tree Team Project 0x1D.C

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,11 +6,10 @@
  * @tree: Pointer to the tree to go through
  * Return: Height
  */
-
 size_t binary_tree_height_b(const binary_tree_t *tree)
 {
-	size_t h_l = 0;
-	size_t h_r = 0;
+	size_t l = 0;
+	size_t r = 0;
 
 	if (tree == NULL)
 	{
@@ -20,10 +19,10 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
 	{
 		if (tree)
 		{
-			h_l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
-			h_r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
+			l = tree->left ? 1 + binary_tree_height_b(tree->left) : 1;
+			r = tree->right ? 1 + binary_tree_height_b(tree->right) : 1;
 		}
-		return ((h_l > h_r) ? l : h_r);
+		return ((l > r) ? l : r);
 	}
 }
 
@@ -37,13 +36,13 @@ size_t binary_tree_height_b(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int right = 0, left = 0, sum = 0;
+	int right = 0, left = 0, total = 0;
 
 	if (tree)
 	{
 		left = ((int)binary_tree_height_b(tree->left));
 		right = ((int)binary_tree_height_b(tree->right));
-		sum = left - right;
+		total = left - right;
 	}
-	return (sum);
+	return (total);
 }
