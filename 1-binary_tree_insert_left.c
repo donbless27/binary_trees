9e637d255873a552997795204b6852eb86d58750
donbless27 <donbless0527@gmail.com>
2023-11-01 05:27:55 -0700

    Binary Tree Team Project 0x1D.C

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,9 +7,9 @@
  *@value: The value to store in the new node
  * Return: Return a pointer to created node, or NULL on failure
  *        or if parent is NULL
- * Description: If parent already has a left-child, the new node
- *    must take its place, and the old left-child must be set
- *    as the left-child of the new node.       
+ * Description: If parent already has a left-child, the new node must
+ *   take its place, and the old left-child must be setas the left-child
+ *   of the new node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
